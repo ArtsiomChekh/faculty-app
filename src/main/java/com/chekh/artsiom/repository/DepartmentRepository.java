cc1c6ed0fb881db75e6c3ec93a08f97f0605d26c
ArtsiomChekh <qbartem@gmail.com>
2023-08-16 22:59:40 +0300

    add SQL request  in DepartmentRepository

--- src/main/java/com/chekh/artsiom/repository/DepartmentRepository.java
+++ src/main/java/com/chekh/artsiom/repository/DepartmentRepository.java
@@ -2,8 +2,7 @@ package com.chekh.artsiom.repository;
 
 import com.chekh.artsiom.model.Department;
 
-import java.util.Map;
-import javax.persistence.MapKey;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +10,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
-    @Query("SELECT d, COUNT(ds) FROM Department d JOIN DepartmentStudent ON d.id = ds.dapartment_id GROUP BY d")
-    @MapKey(name = "d")
-    Map<Department, Long> findDepartmentStudentCount();
+    @Query("Select d, count(ds.student) \n"
+        + "From Department d\n"
+        + "Left Join DepartmentStudent ds ON d.id = ds.department\n"
+        + "Left Join Student s ON s.id = ds.student\n"
+        + "GROUP BY d")
+    List<Object[]> findDepartmentStudentCount();
 
 }
