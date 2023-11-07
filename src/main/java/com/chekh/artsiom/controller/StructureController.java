022c047a739567593842ce638df7628d7a598565
artsiomchekh <qbartem@gmail.com>
2023-11-07 13:54:45 +0300

    Layout: display information about the faculty.

--- src/main/java/com/chekh/artsiom/controller/StructureController.java
+++ src/main/java/com/chekh/artsiom/controller/StructureController.java
@@ -1,10 +1,16 @@
 package com.chekh.artsiom.controller;
 
+import com.chekh.artsiom.model.Subject;
 import com.chekh.artsiom.service.DepartmentService;
+import com.chekh.artsiom.service.SubjectService;
+import com.chekh.artsiom.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class StructureController {
@@ -12,11 +18,16 @@ public class StructureController {
     @Autowired
     private DepartmentService departmentService;
 
+    @Autowired
+    private SubjectService subjectService;
+
+    @Autowired
+    private TeacherService teacherService;
+
     @GetMapping(value = {"/structure"})
-    public String showStructurePage(Model model) {
+    public String showStructurePage (Model model) {
         model.addAttribute("departments", departmentService.getAllDepartments());
+        model.addAttribute("subjects", subjectService.getAllSubjects());
         return "structure";
     }
-
-
 }
