e7ba517219495e08e47d41c421a4e6afa8c04423
ArtsiomChekh <qbartem@gmail.com>
2023-11-23 22:50:22 +0300

    Added TeacherController.

---
+++ src/main/java/com/chekh/artsiom/controller/TeacherController.java
@@ -0,0 +1,35 @@
+package com.chekh.artsiom.controller;
+
+import com.chekh.artsiom.model.Department;
+import com.chekh.artsiom.model.Subject;
+import com.chekh.artsiom.service.DepartmentService;
+import com.chekh.artsiom.service.SubjectService;
+import com.chekh.artsiom.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class TeacherController {
+
+    @Autowired
+    TeacherService teacherService;
+
+    @Autowired
+    DepartmentService departmentService;
+
+    @Autowired
+    SubjectService subjectService;
+
+    @GetMapping("/for-teacher")
+    public String showForTeacherPage(Model model) {
+        List<Subject> subjects = subjectService.getAllSubjects();
+        List<Department> departments = departmentService.getAllDepartments();
+        model.addAttribute("subjects", subjects);
+        model.addAttribute("departments", departments);
+        return "for-teacher";
+    }
+}
