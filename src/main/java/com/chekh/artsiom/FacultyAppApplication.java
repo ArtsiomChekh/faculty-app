d03368d8c3d1e5ec53f40f607131454e5529cd49
ArtsiomChekh <qbartem@gmail.com>
2023-12-12 00:01:07 +0300

    Hided database MySql login info.

--- src/main/java/com/chekh/artsiom/FacultyAppApplication.java
+++ src/main/java/com/chekh/artsiom/FacultyAppApplication.java
@@ -2,8 +2,10 @@ package com.chekh.artsiom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:database.properties")
 public class FacultyAppApplication {
 
     public static void main(String[] args) {
