8c34a546169729527cccf683fa56e2d9424e996f
artsiomchekh <qbartem@gmail.com>
2023-12-23 21:17:52 +0300

    Updated SecurityConfiguration.

--- src/main/java/com/chekh/artsiom/config/SecurityConfiguration.java
+++ src/main/java/com/chekh/artsiom/config/SecurityConfiguration.java
@@ -1,36 +1,57 @@
 package com.chekh.artsiom.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/index", "/", "/css/**", "/images/**");
-    }
+    @Lazy
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers("/register", "/index", "/", "/css/**", "/images/**");
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/register", "/home", "/", "/css/**", "/images/**").permitAll()
+                        .requestMatchers("/admin").hasAuthority("Admin")
+                        .requestMatchers("/student").hasAuthority("Student")
+                        .requestMatchers("/teacher").hasAuthority("Teacher")
+                        .requestMatchers("/common").hasAnyAuthority("Admin", "Student", "Teacher")
                         .anyRequest().authenticated()
                 )
                 .formLogin((login) -> login
                         .loginPage("/login")
                         .permitAll()
-                );
+                        .defaultSuccessUrl("/home", true)
+                )
+
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                )
+
+                .exceptionHandling((ex -> ex
+                        .accessDeniedPage("/accessDenied")))
+
+                .authenticationProvider(authenticationProvider());
         return http.build();
     }
 
@@ -40,13 +61,10 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return authenticationProvider;
     }
 }
