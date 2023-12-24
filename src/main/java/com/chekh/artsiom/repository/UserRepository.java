988012b589fc40d8878d170bf21114ff366ebee4
artsiomchekh <qbartem@gmail.com>
2023-12-24 15:36:52 +0300

    Added saveUser() test.

--- src/main/java/com/chekh/artsiom/repository/UserRepository.java
+++ src/main/java/com/chekh/artsiom/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.chekh.artsiom.repository;
 
 import com.chekh.artsiom.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
 }
